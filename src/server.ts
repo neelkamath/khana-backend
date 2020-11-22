02b86d457d44c5e5add98714998e232f2f5ebcdf
neelkamath <neelkamath@icloud.com>
2020-11-22 15:38:49 +0530

    Enable CORS

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import {setUpDb} from './db';
 import {setUpHttpApi} from './http-api';
 import {setUpWebSocketApi} from './ws-api';
 
 setUpDb().catch(console.error);
 const app = express().use(bodyParser.json());
+app.use(cors());
 setUpHttpApi(app);
 setUpWebSocketApi(app);
